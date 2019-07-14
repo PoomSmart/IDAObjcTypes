9f53d0c374a1b41dd5df463f8c6cd1d1e8385550
PoomSmart <thatchapon.unp@outlook.com>
2019-07-14 13:09:56 +0700

    Added IOKit - HID

--- IOKit/IOKit.h
+++ IOKit/IOKit.h
@@ -2,12 +2,90 @@
 #import "Types.h"
 
 kern_return_t IOObjectRelease(io_object_t object);
-kern_return_t _imp__IOObjectRelease(io_object_t object);
 kern_return_t IOObjectRetain(io_object_t object);
-kern_return_t _imp__IOObjectRetain(io_object_t object);
 
 io_service_t IOServiceGetMatchingService(mach_port_t masterPort, CFDictionaryRef matching);
-io_service_t _imp__IOServiceGetMatchingService(mach_port_t masterPort, CFDictionaryRef matching);
+
+CFTypeID IOHIDEventGetTypeID(void);
 
 CFMutableDictionaryRef IOServiceMatching(const char* name);
-CFMutableDictionaryRef _imp__IOServiceMatching(const char* name);
+
+IOHIDEventRef IOHIDEventCreateCopy(CFAllocatorRef allocator, IOHIDEventRef event);
+IOHIDEventRef IOHIDEventCreate(CFAllocatorRef allocator, IOHIDEventType type, AbsoluteTime timeStamp, IOOptionBits options);
+
+CFMutableDataRef IOHIDEventCreateData(CFAllocatorRef allocator, IOHIDEventRef event);
+IOHIDEventRef IOHIDEventCreateWithData(CFAllocatorRef allocator, CFDataRef data);
+
+IOHIDEventRef IOHIDEventCreateProgressEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t eventType, IOHIDFloat level, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateVendorDefinedEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint16_t usagePage, uint16_t usage, uint32_t version, const uint8_t* data, uint32_t length, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateSwipeEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDSwipeMask swipeMask, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateDigitizerEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDDigitizerTransducerType type, uint32_t index, uint32_t identity, uint32_t eventMask, uint32_t buttonMask, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOHIDFloat tipPressure, IOHIDFloat barrelPressure, Boolean range, Boolean touch, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateDigitizerFingerEventWithQuality(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t index, uint32_t identity, uint32_t eventMask, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOHIDFloat tipPressure, IOHIDFloat twist, IOHIDFloat minorRadius, IOHIDFloat majorRadius, IOHIDFloat quality, IOHIDFloat density, IOHIDFloat irregularity, Boolean range, Boolean touch, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateDigitizerFingerEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t index, uint32_t identity, uint32_t eventMask, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOHIDFloat tipPressure, IOHIDFloat twist, Boolean range, Boolean touch, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateDigitizerStylusEventWithPolarOrientation(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t index, uint32_t identity, uint32_t eventMask, uint32_t buttonMask, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOHIDFloat tipPressure, IOHIDFloat barrelPressure, IOHIDFloat twist, IOHIDFloat altitude, IOHIDFloat azimuth,  Boolean range, Boolean invert, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateDigitizerStylusEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t index, uint32_t identity, uint32_t eventMask, uint32_t buttonMask, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOHIDFloat tipPressure, IOHIDFloat barrelPressure, IOHIDFloat twist, IOHIDFloat altitude, IOHIDFloat azimuth, Boolean range, Boolean invert, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateProximtyEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDProximityDetectionMask detectionMask, IOOptionBits options);
+#define IOHIDEventCreateProximityEvent IOHIDEventCreateProximtyEvent
+IOHIDEventRef IOHIDEventCreateAmbientLightSensorEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat level, IOOptionBits options);
+
+
+IOHIDEventRef IOHIDEventCreateMouseEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, uint32_t buttonMask, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateMouseEventWithPressure(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, uint32_t buttonMask, IOHIDFloat pressure, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateButtonEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t buttonMask, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateButtonEventWithPressure(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint32_t buttonMask, IOHIDFloat pressure, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventCreateKeyboardEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, uint16_t usagePage, uint16_t usage, Boolean down, IOHIDEventOptionBits flags);
+
+IOHIDEventRef IOHIDEventCreateAccelerometerEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreatePolarOrientationEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateOrientationEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateVelocityEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateScaleEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateScrollEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateRotationEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+IOHIDEventRef IOHIDEventCreateTranslationEvent(CFAllocatorRef allocator, AbsoluteTime timeStamp, IOHIDFloat x, IOHIDFloat y, IOHIDFloat z, IOOptionBits options);
+
+IOHIDEventType IOHIDEventGetType(IOHIDEventRef event);
+
+AbsoluteTime IOHIDEventGetTimeStamp(IOHIDEventRef event);
+void IOHIDEventSetTimeStamp(IOHIDEventRef event, AbsoluteTime timeStamp);
+
+uint32_t IOHIDEventGetEventFlags(IOHIDEventRef event);
+void IOHIDEventSetEventFlags(IOHIDEventRef event, uint32_t eventFlags);
+Boolean IOHIDEventIsAbsolute(IOHIDEventRef event);
+
+IOHIDEventRef IOHIDEventGetParent(IOHIDEventRef event);
+CFArrayRef IOHIDEventGetChildren(IOHIDEventRef event);
+
+void IOHIDEventGetVendorDefinedData(IOHIDEventRef event, uint32_t* length, uint8_t** data);
+
+IOHIDEventRef IOHIDEventGetEventWithOptions(IOHIDEventRef event, IOHIDEventType type, IOOptionBits options);
+
+IOHIDEventRef IOHIDEventGetEvent(IOHIDEventRef event, IOHIDEventType type);
+
+void IOHIDEventSetFloatValueWithOptions(IOHIDEventRef event, IOHIDEventField field, IOHIDFloat value, IOOptionBits options);
+void IOHIDEventSetFloatValue(IOHIDEventRef event, IOHIDEventField field, IOHIDFloat value);
+void IOHIDEventSetIntegerValueWithOptions(IOHIDEventRef event, IOHIDEventField field, int value, IOOptionBits options);
+void IOHIDEventSetIntegerValue(IOHIDEventRef event, IOHIDEventField field, int value);
+void IOHIDEventSetPositionWithOptions(IOHIDEventRef event, IOHIDEventField field, IOHID3DPoint position, IOOptionBits options);
+void IOHIDEventSetPosition(IOHIDEventRef event, IOHIDEventField field, IOHID3DPoint position);
+
+IOHIDFloat IOHIDEventGetFloatValueWithOptions(IOHIDEventRef event, IOHIDEventField field, IOOptionBits options);
+IOHIDFloat IOHIDEventGetFloatValue(IOHIDEventRef event, IOHIDEventField field);
+int IOHIDEventGetIntegerValueWithOptions(IOHIDEventRef event, IOHIDEventField field, IOOptionBits options);
+int IOHIDEventGetIntegerValue(IOHIDEventRef event, IOHIDEventField field);
+IOHID3DPoint IOHIDEventGetPositionWithOptions(IOHIDEventRef event, IOHIDEventField field, IOOptionBits options);
+IOHID3DPoint IOHIDEventGetPosition(IOHIDEventRef event, IOHIDEventField field);
+
+Boolean IOHIDEventConformsToWithOptions(IOHIDEventRef event, IOHIDEventType type, IOOptionBits options);
+Boolean IOHIDEventConformsTo(IOHIDEventRef event, IOHIDEventType type);
+
+void IOHIDEventRemoveEvent(IOHIDEventRef event, IOHIDEventRef childEvent);
+void IOHIDEventAppendEvent(IOHIDEventRef event, IOHIDEventRef childEvent);
--- IOKit/Types.h
+++ IOKit/Types.h
@@ -1,10 +1,205 @@
 #ifndef IOKIT_H_
 #define IOKIT_H_
 
+#define IOHIDEventFieldBase(type) (type << 16)
+
 typedef OSObject* io_object_t;
 
 typedef io_object_t* io_service_t;
 
+typedef UInt32 IOOptionBits;
+typedef SInt32 IOFixed;
+typedef UInt32 IOVersion;
+typedef UInt32 IOItemCount;
+typedef UInt32 IOCacheMode;
+
+typedef UInt32 IOByteCount32;
+typedef UInt64 IOByteCount64;
+
+typedef UInt32 IOPhysicalAddress32;
+typedef UInt64 IOPhysicalAddress64;
+typedef UInt32 IOPhysicalLength32;
+typedef UInt64 IOPhysicalLength64;
+
+#ifdef __LP64__
+typedef mach_vm_address_t IOVirtualAddress;
+#else
+typedef vm_address_t IOVirtualAddress;
+#endif
+
+typedef IOVirtualAddress IOLogicalAddress;
+
+typedef struct __IOHIDEvent* IOHIDEventRef;
+
 const mach_port_t kIOMasterPortDefault;
+
+typedef uint32_t IOHIDEventType; enum IOHIDEventType {
+    kIOHIDEventTypeNULL,
+    kIOHIDEventTypeVendorDefined,
+    kIOHIDEventTypeButton,
+    kIOHIDEventTypeKeyboard,
+    kIOHIDEventTypeTranslation,
+    kIOHIDEventTypeRotation,
+    kIOHIDEventTypeScroll,
+    kIOHIDEventTypeScale,
+    kIOHIDEventTypeZoom,
+    kIOHIDEventTypeVelocity,
+    kIOHIDEventTypeOrientation,
+    kIOHIDEventTypeDigitizer,
+    kIOHIDEventTypeAmbientLightSensor,
+    kIOHIDEventTypeAccelerometer,
+    kIOHIDEventTypeProximity,
+    kIOHIDEventTypeTemperature,
+    kIOHIDEventTypeNavigationSwipe,
+    kIOHIDEventTypePointer,
+    kIOHIDEventTypeProgress,
+    kIOHIDEventTypeMultiAxisPointer,
+    kIOHIDEventTypeGyro,
+    kIOHIDEventTypeCompass,
+    kIOHIDEventTypeZoomToggle,
+    kIOHIDEventTypeDockSwipe,
+    kIOHIDEventTypeSymbolicHotKey,
+    kIOHIDEventTypePower,
+    kIOHIDEventTypeLED,
+    kIOHIDEventTypeFluidTouchGesture,
+    kIOHIDEventTypeBoundaryScroll,
+    kIOHIDEventTypeBiometric,
+    kIOHIDEventTypeUnicode,
+    kIOHIDEventTypeAtmosphericPressure,
+    kIOHIDEventTypeForce,
+    kIOHIDEventTypeMotionActivity,
+    kIOHIDEventTypeMotionGesture,
+    kIOHIDEventTypeGameController,
+    kIOHIDEventTypeHumidity,
+    kIOHIDEventTypeCollection,
+    kIOHIDEventTypeBrightness,
+    kIOHIDEventTypeGenericGesture,
+    kIOHIDEventTypeCount,
+
+    kIOHIDEventTypeSwipe = kIOHIDEventTypeNavigationSwipe,
+    kIOHIDEventTypeMouse = kIOHIDEventTypePointer
+} IOHIDEventType;
+
+typedef uint32_t IOHIDAccelerometerType; enum IOHIDAccelerometerType {
+    kIOHIDAccelerometerTypeNormal   = 0,
+    kIOHIDAccelerometerTypeShake    = 1
+} IOHIDAccelerometerType;
+
+typedef uint32_t IOHIDSwipeMask; enum IOHIDSwipeMask {
+    kIOHIDSwipeUp                             = 0x00000001,
+    kIOHIDSwipeDown                           = 0x00000002,
+    kIOHIDSwipeLeft                           = 0x00000004,
+    kIOHIDSwipeRight                          = 0x00000008,
+} IOHIDSwipeMask;
+
+typedef uint32_t IOHIDProximityDetectionMask; enum IOHIDProximityDetectionMask {
+    kIOHIDProximityDetectionLargeBodyContact                = 0x0001,
+    kIOHIDProximityDetectionLargeBodyFarField               = 0x0002,
+    kIOHIDProximityDetectionIrregularObjects                = 0x0004,
+    kIOHIDProximityDetectionEdgeStraddling                  = 0x0008,
+    kIOHIDProximityDetectionFlatFingerClasp                 = 0x0010,
+    kIOHIDProximityDetectionFingerTouch                     = 0x0020,
+    kIOHIDProximityDetectionReceiver                        = 0x0040,
+    kIOHIDProximityDetectionSmallObjectsHovering            = 0x0080,
+    kIOHIDProximityDetectionReceiverCrude                   = 0x0100
+} IOHIDProximityDetectionMask;
+
+typedef uint32_t IOHIDDigitizerTransducerType; enum IOHIDDigitizerTransducerType {   
+    kIOHIDDigitizerTransducerTypeStylus  = 0x20,
+    kIOHIDDigitizerTransducerTypePuck,
+    kIOHIDDigitizerTransducerTypeFinger,
+    kIOHIDDigitizerTransducerTypeHand
+} IOHIDDigitizerTransducerType;
+
+typedef uint32_t IOHIDDigitizerEventMask; enum IOHIDDigitizerEventMask {
+    kIOHIDDigitizerEventRange                               = 0x00000001,
+    kIOHIDDigitizerEventTouch                               = 0x00000002,
+    kIOHIDDigitizerEventPosition                            = 0x00000004,
+    kIOHIDDigitizerEventStop                                = 0x00000008,
+    kIOHIDDigitizerEventPeak                                = 0x00000010,
+    kIOHIDDigitizerEventIdentity                            = 0x00000020,
+    kIOHIDDigitizerEventAttribute                           = 0x00000040,
+    kIOHIDDigitizerEventCancel                              = 0x00000080,
+    kIOHIDDigitizerEventStart                               = 0x00000100,
+    kIOHIDDigitizerEventResting                             = 0x00000200,
+    kIOHIDDigitizerEventSwipeUp                             = 0x01000000,
+    kIOHIDDigitizerEventSwipeDown                           = 0x02000000,
+    kIOHIDDigitizerEventSwipeLeft                           = 0x04000000,
+    kIOHIDDigitizerEventSwipeRight                          = 0x08000000,
+    kIOHIDDigitizerEventSwipeMask                           = 0xFF000000,
+} IOHIDDigitizerEventMask;
+
+typedef uint32_t IOHIDEventOptionBits; enum IOHIDEventOptionBits {
+    kIOHIDEventOptionIsAbsolute                             = 0x00000001,
+    kIOHIDEventOptionIsCollection                           = 0x00000002,
+    kIOHIDEventOptionPixelUnits                             = 0x00000004
+} IOHIDEventOptionBits;
+
+#ifdef __LP64__
+typedef double IOHIDFloat;
+#else
+typedef float IOHIDFloat;
+#endif
+
+typedef struct _IOHID3DPoint {
+    IOHIDFloat  x;
+    IOHIDFloat  y;
+    IOHIDFloat  z;
+} IOHID3DPoint;
+
+typedef uint32_t IOHIDEventField; enum IOHIDEventField {
+    kIOHIDEventFieldAccelerometerX = IOHIDEventFieldBase(kIOHIDEventTypeAccelerometer),
+    kIOHIDEventFieldAccelerometerY,
+    kIOHIDEventFieldAccelerometerZ,
+    kIOHIDEventFieldAccelerometerType,
+    kIOHIDEventFieldMouseX = IOHIDEventFieldBase(kIOHIDEventTypeMouse),
+    kIOHIDEventFieldMouseY,
+    kIOHIDEventFieldMouseZ,
+    kIOHIDEventFieldMouseButtonMask,
+    kIOHIDEventFieldMouseNumber,
+    kIOHIDEventFieldMouseClickCount,
+    kIOHIDEventFieldMousePressure,
+    kIOHIDEventFieldAmbientLightSensorLevel = IOHIDEventFieldBase(kIOHIDEventTypeAmbientLightSensor),
+    kIOHIDEventFieldAmbientLightSensorRawChannel0,
+    kIOHIDEventFieldAmbientLightSensorRawChannel1,
+    kIOHIDEventFieldAmbientLightDisplayBrightnessChanged,
+    kIOHIDEventFieldTemperatureLevel = IOHIDEventFieldBase(kIOHIDEventTypeTemperature),
+    kIOHIDEventFieldProximityDetectionMask = IOHIDEventFieldBase(kIOHIDEventTypeProximity),
+    kIOHIDEventFieldOrientationRadius   = IOHIDEventFieldBase(kIOHIDEventTypeOrientation),
+    kIOHIDEventFieldOrientationAzimuth,
+    kIOHIDEventFieldOrientationAltitude,
+    kIOHIDEventFieldKeyboardUsagePage = IOHIDEventFieldBase(kIOHIDEventTypeKeyboard),
+    kIOHIDEventFieldKeyboardUsage,
+    kIOHIDEventFieldKeyboardDown,
+    kIOHIDEventFieldKeyboardRepeat,
+    kIOHIDEventFieldDigitizerX = IOHIDEventFieldBase(kIOHIDEventTypeDigitizer),
+    kIOHIDEventFieldDigitizerY,
+    kIOHIDEventFieldDigitizerZ,
+    kIOHIDEventFieldDigitizerButtonMask,
+    kIOHIDEventFieldDigitizerType,
+    kIOHIDEventFieldDigitizerIndex,
+    kIOHIDEventFieldDigitizerIdentity,
+    kIOHIDEventFieldDigitizerEventMask,
+    kIOHIDEventFieldDigitizerRange,
+    kIOHIDEventFieldDigitizerTouch,
+    kIOHIDEventFieldDigitizerPressure,
+    kIOHIDEventFieldDigitizerBarrelPressure,
+    kIOHIDEventFieldDigitizerTwist,
+    kIOHIDEventFieldDigitizerTiltX,
+    kIOHIDEventFieldDigitizerTiltY,
+    kIOHIDEventFieldDigitizerAltitude,
+    kIOHIDEventFieldDigitizerAzimuth,
+    kIOHIDEventFieldDigitizerQuality,
+    kIOHIDEventFieldDigitizerDensity,
+    kIOHIDEventFieldDigitizerIrregularity,
+    kIOHIDEventFieldDigitizerMajorRadius,
+    kIOHIDEventFieldDigitizerMinorRadius,
+    kIOHIDEventFieldDigitizerCollection,
+    kIOHIDEventFieldDigitizerCollectionChord,
+    kIOHIDEventFieldDigitizerChildEventMask,
+    kIOHIDEventFieldSwipeMask = IOHIDEventFieldBase(kIOHIDEventTypeSwipe),
+    kIOHIDEventFieldProgressEventType = IOHIDEventFieldBase(kIOHIDEventTypeProgress),
+    kIOHIDEventFieldProgressLevel
+} IOHIDEventField;
 
 #endif
