56d81a9bd58c58024b958d1f17bfa4f2947b731b
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2020-03-21 19:36:47 +0700

    Update MobileGestalt.h

--- MobileGestalt/MobileGestalt.h
+++ MobileGestalt/MobileGestalt.h
@@ -6,7 +6,7 @@ SInt32 MGGetSInt32Answer(CFStringRef key, SInt32 defaultVal);
 Float32 MGGetFloat32Answer(CFStringRef key, Float32 defaultVal);
 
 bool MGGetBoolAnswer(CFStringRef key);
-Boolean MGIsQuestionValid(CFStringRef key);
+bool MGIsQuestionValid(CFStringRef key);
 
 CFTypeRef MGCopyAnswer(CFStringRef question, CFDictionaryRef options);
 CFPropertyListRef MGCopyMultipleAnswers(CFArrayRef questions, int __unknown0);
