59008d2ef06a5d212dfe8392fd46feebd52e1f26
PoomSmart <thatchapon.unp@outlook.com>
2020-04-19 16:04:27 +0700

    I was wrong

--- Darwin/Darwin.h
+++ Darwin/Darwin.h
@@ -45,7 +45,7 @@ void *__strncpy_chk(char *dest, char *src, size_t len, size_t dstlen);
 
 void abort_report_np(const char *, ...);
 void backtrace_symbols_fd(void * const *array, int size, int fd);
-void dispatch_once(dispatch_block_t block);
+void dispatch_once(dispatch_once_t *predicate, dispatch_block_t block);
 void dispatch_apply(size_t iterations, dispatch_queue_t queue, void (*block)(size_t));
 void dispatch_assert_queue(dispatch_queue_t queue);
 void dispatch_assert_queue$V2(dispatch_queue_t queue);
