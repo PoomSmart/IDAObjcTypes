77f66cf1f56f7257ef5559e07826e45c9f07fc3e
absidue <48293849+absidue@users.noreply.github.com>
2021-08-10 23:14:34 +0200

    Add more MediaRemote types and functions

--- MediaRemote/MediaRemote.h
+++ MediaRemote/MediaRemote.h
@@ -15,13 +15,17 @@ int MRMediaRemoteSelectSourceWithID(CFStringRef identifier);
 
 void MRMediaRemoteBeginRouteDiscovery();
 void MRMediaRemoteEndRouteDiscovery();
+void MRMediaRemoteGetMediaPlaybackVolume(dispatch_queue_t queue, MRMediaRemoteGetMediaPlaybackVolumeCompletion completion);
 void MRMediaRemoteGetNowPlayingApplicationIsPlaying(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingApplicationIsPlayingCompletion completion);
 void MRMediaRemoteGetNowPlayingApplicationPID(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingApplicationPIDCompletion completion);
 void MRMediaRemoteGetNowPlayingInfo(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingInfoCompletion completion);
+void MRMediaRemoteGetSavedAVRoutePassword(CFStringRef route, dispatch_queue_t queue, MRMediaRemoteGetSavedAVRoutePasswordCompletion completion);
+void MRMediaRemoteGetSupportedCommands(dispatch_queue_t queue, MRMediaRemoteGetSupportedCommandsCompletion completion);
 void MRMediaRemoteKeepAlive();
 void MRMediaRemoteRegisterForNowPlayingNotifications(dispatch_queue_t queue);
 void MRMediaRemoteSetElapsedTime(double elapsedTime);
 void MRMediaRemoteSetElapsedTime(double time);
+void MRMediaRemoteSetMediaPlaybackVolume(float volume, Boolean bypassVolumeWarning, dispatch_queue_t queue, MRMediaRemoteSetMediaPlaybackVolumeCompletion completion);
 void MRMediaRemoteSetNowPlayingApplicationOverrideEnabled(Boolean enabled);
 void MRMediaRemoteSetNowPlayingInfo(CFDictionaryRef information);
 void MRMediaRemoteSetPickedRouteWithPassword(CFStringRef route, CFStringRef password);
--- MediaRemote/Types.h
+++ MediaRemote/Types.h
@@ -5,42 +5,90 @@
 #import "../Foundation/Types.h"
 
 PS_ENUM(NSInteger, MRMediaRemoteCommand) {
-    MRMediaRemoteCommandPlay,
-    MRMediaRemoteCommandPause,
-    MRMediaRemoteCommandTogglePlayPause,
-    MRMediaRemoteCommandStop,
-    MRMediaRemoteCommandNextTrack,
-    MRMediaRemoteCommandPreviousTrack,
-    MRMediaRemoteCommandAdvanceShuffleMode,
-    MRMediaRemoteCommandAdvanceRepeatMode,
-    MRMediaRemoteCommandBeginFastForward,
-    MRMediaRemoteCommandEndFastForward,
-    MRMediaRemoteCommandBeginRewind,
-    MRMediaRemoteCommandEndRewind,
-    MRMediaRemoteCommandRewind15Seconds,
-    MRMediaRemoteCommandFastForward15Seconds,
-    MRMediaRemoteCommandRewind30Seconds,
-    MRMediaRemoteCommandFastForward30Seconds,
-    MRMediaRemoteCommandToggleRecord,
-    MRMediaRemoteCommandSkipForward,
-    MRMediaRemoteCommandSkipBackward,
-    MRMediaRemoteCommandChangePlaybackRate,
+    MRMediaRemoteCommandPlay = 0,
+    MRMediaRemoteCommandPause = 1,
+    MRMediaRemoteCommandTogglePlayPause = 2,
+    MRMediaRemoteCommandStop = 3,
+    MRMediaRemoteCommandNextTrack = 4,
+    MRMediaRemoteCommandPreviousTrack = 5,
+    MRMediaRemoteCommandAdvanceShuffleMode = 6,
+    MRMediaRemoteCommandAdvanceRepeatMode = 7,
+    MRMediaRemoteCommandBeginFastForward = 8,
+    MRMediaRemoteCommandEndFastForward = 9,
+    MRMediaRemoteCommandBeginRewind = 10,
+    MRMediaRemoteCommandEndRewind = 11,
+    MRMediaRemoteCommandRewind15Seconds = 12,
+    MRMediaRemoteCommandFastForward15Seconds = 13,
+    MRMediaRemoteCommandRewind30Seconds = 14,
+    MRMediaRemoteCommandFastForward30Seconds = 15,
+    MRMediaRemoteCommandToggleRecord = 16,
+    MRMediaRemoteCommandSkipForward = 17,
+    MRMediaRemoteCommandSkipBackward = 18,
+    MRMediaRemoteCommandChangePlaybackRate = 19,
 
-    MRMediaRemoteCommandRateTrack,
-    MRMediaRemoteCommandLikeTrack,
-    MRMediaRemoteCommandDislikeTrack,
-    MRMediaRemoteCommandBookmarkTrack,
+    MRMediaRemoteCommandRateTrack = 20,
+    MRMediaRemoteCommandLikeTrack = 21,
+    MRMediaRemoteCommandDislikeTrack = 22,
+    MRMediaRemoteCommandBookmarkTrack = 23,
 
-    MRMediaRemoteCommandSeekToPlaybackPosition,
-    MRMediaRemoteCommandChangeRepeatMode,
-    MRMediaRemoteCommandChangeShuffleMode,
-    MRMediaRemoteCommandEnableLanguageOption,
-    MRMediaRemoteCommandDisableLanguageOption
+    MRMediaRemoteCommandSeekToPlaybackPosition = 24,
+    MRMediaRemoteCommandChangeRepeatMode = 25,
+    MRMediaRemoteCommandChangeShuffleMode = 26,
+    MRMediaRemoteCommandEnableLanguageOption = 27,
+    MRMediaRemoteCommandDisableLanguageOption = 28,
+
+    MRMediaRemoteCommandNextChapter = 100,
+    MRMediaRemoteCommandPreviousChapter = 101,
+    MRMediaRemoteCommandNextAlbum = 102,
+    MRMediaRemoteCommandPreviousAlbum = 103,
+    MRMediaRemoteCommandNextPlaylist = 104,
+    MRMediaRemoteCommandPreviousPlaylist = 105
+
+    MRMediaRemoteCommandBanTrack = 106,
+    MRMediaRemoteCommandAddTrackToWishList = 107,
+    MRMediaRemoteCommandRemoveTrackFromWishList = 108,
+    MRMediaRemoteCommandNextInContext = 109,
+    MRMediaRemoteCommandPreviousInContext = 110,
+
+    MRMediaRemoteCommandMenu = 111,
+    MRMediaRemoteCommandSelect = 112,
+    MRMediaRemoteCommandUpArrow = 113,
+    MRMediaRemoteCommandDownArrow = 114,
+    MRMediaRemoteCommandBacklightOff = 115,
+
+    MRMediaRemoteCommandResetPlaybackTimeout = 116,
+    MRMediaRemoteCommandBuyTrack = 117,
+    MRMediaRemoteCommandBuyAlbum = 118,
+    MRMediaRemoteCommandPreOrderAlbum = 119,
+    MRMediaRemoteCommandCancelDownload = 120,
+    MRMediaRemoteCommandCreateRadioStation = 121,
+    MRMediaRemoteCommandSetPlaybackQueue = 122,
+    MRMediaRemoteCommandPresentUpNext = 123,
+    MRMediaRemoteCommandSetSleepTimer = 124,
+    MRMediaRemoteCommandInsertIntoPlaybackQueue = 125,
+    MRMediaRemoteCommandPresentSharingOptions = 126,
+
+    MRMediaRemoteCommandAddNowPlayingItemToLibrary = 127,
+    MRMediaRemoteCommandAddItemToLibrary = 128,
+    MRMediaRemoteCommandRemoveFromPlaybackQueue = 129,
+    MRMediaRemoteCommandReorderPlaybackQueue = 130,
+    MRMediaRemoteCommandPlayItemInPlaybackQueue = 131,
+    MRMediaRemoteCommandPrepareForSetQueue = 132,
+    MRMediaRemoteCommandSetPlaybackSession = 133,
+    MRMediaRemoteCommandReshuffle = 134,
+    MRMediaRemoteCommandChangeQueueEndAction = 135,
+    MRMediaRemoteCommandPreloadPlaybackSession = 136,
+    MRMediaRemoteCommandSetPriorityForPlaybackSession = 137,
+    MRMediaRemoteCommandDiscardPlaybackSession = 138
 };
 
+typedef void (*MRMediaRemoteGetMediaPlaybackVolumeCompletion)(float volume);
 typedef void (*MRMediaRemoteGetNowPlayingInfoCompletion)(CFDictionaryRef information);
 typedef void (*MRMediaRemoteGetNowPlayingApplicationPIDCompletion)(int PID);
 typedef void (*MRMediaRemoteGetNowPlayingApplicationIsPlayingCompletion)(Boolean isPlaying);
+typedef void (*MRMediaRemoteGetSavedAVRoutePasswordCompletion)(CFStringRef password);
+typedef void (*MRMediaRemoteGetSupportedCommandsCompletion)(CFArrayRef commands);
+typedef void (*MRMediaRemoteSetMediaPlaybackVolumeCompletion)(CFErrorRef error);
 
 CFStringRef kMRMediaRemoteNowPlayingInfoDidChangeNotification;
 CFStringRef kMRMediaRemoteNowPlayingPlaybackQueueDidChangeNotification;
