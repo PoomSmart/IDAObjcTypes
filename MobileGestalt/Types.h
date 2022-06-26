707fa4712791ac1d920c854233f678eb02520028
PoomSmart <thatchapon.unp@outlook.com>
2022-06-26 19:57:43 +0700

    Added more device classes

--- MobileGestalt/Types.h
+++ MobileGestalt/Types.h
@@ -1,3 +1,17 @@
 typedef enum {
     MGProductTypeUnknown = -1,
 } MGProductType;
+
+typedef enum {
+    MGDeviceClassInvalid            = -1,
+    MGDeviceClassiPhone             = 1,
+    MGDeviceClassiPod               = 2,
+    MGDeviceClassiPad               = 3,
+    MGDeviceClassAppleTV            = 4,
+    MGDeviceClassiFPGA              = 5,
+    MGDeviceClassWatch              = 6,
+    MGDeviceClassAudioAccessory     = 7,
+    MGDeviceClassiBridge            = 8,
+    MGDeviceClassMac                = 9,
+    MGDeviceClassAppleDisplay       = 10,
+} MGDeviceClass;
