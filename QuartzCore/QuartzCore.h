1d9fab2d4787b5d56fd831e2b42946548ba29537
PoomSmart <thatchapon.unp@outlook.com>
2022-12-29 12:43:01 +0700

    Add CAFrameRateRange things

---
+++ QuartzCore/QuartzCore.h
@@ -0,0 +1,5 @@
+#import "Types.h"
+
+bool CAFrameRateRangeIsEqualToRange(CAFrameRateRange range, CAFrameRateRange other);
+
+CAFrameRateRange CAFrameRateRangeMake(float minimum, float maximum, float preferred);
--- QuartzCore/Types.h
+++ QuartzCore/Types.h
@@ -3,3 +3,5 @@ typedef struct CAFrameRateRange {
     float maximum;
     float preferred;
 } CAFrameRateRange;
+
+extern const CAFrameRateRange CAFrameRateRangeDefault;
